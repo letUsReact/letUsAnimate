fea97bc2ddda74d49163cd4a21778fb6b4dbc0be
Prerna Budhraja <prernabudhraja8@gmail.com>
2020-11-30 20:55:38 +0530

    typing-effect

---
+++ #2/prernabudh/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Typing from "./typing/Typing";
+
+function App() {
+  return (
+    <div className="App">
+      <Typing></Typing>
+    </div>
+  );
+}
+
+export default App;
---
+++ #2/prernabudh/src/typing/Typing.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Typing.css";
+
+const Typing = () => {
+  return (
+    <div className="typing-container">
+      <span className="dot dot-1"></span>
+      <span className="dot dot-2"></span>
+      <span className="dot dot-3"></span>
+    </div>
+  );
+};
+
+export default Typing;
